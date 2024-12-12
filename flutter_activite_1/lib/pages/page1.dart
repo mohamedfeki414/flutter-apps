989bde8832ff942d0332dd473b9494e70fac962a
feki_mohamed <medfeki33@gmail.com>
2024-12-12 09:33:52 +0100

    *

--- flutter_activite_1/lib/pages/page1.dart
+++ flutter_activite_1/lib/pages/page1.dart
@@ -5,7 +5,13 @@ class MaPremierPage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text("Application Flutter"),
+        backgroundColor: const Color.fromARGB(255, 7, 179, 86),
+      ),
+      body:const Text("bonjour iit")
+    );
      
   }
 }
