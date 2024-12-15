90e528b87af24a052ed62a37fe74d9ea7f0d808c
feki_mohamed <medfeki33@gmail.com>
2024-12-15 20:38:53 +0100

    **

--- tp1/activité2/activite2/lib/main.dart
+++ tp1/activité2/activite2/lib/main.dart
@@ -1,32 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const MaterialApp(
+      home: Test(),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Test extends StatelessWidget {
+  const Test({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Ma première Application'),
-          centerTitle: true,
-          backgroundColor: Colors.blueGrey, // Couleur de l'AppBar
-        ),
-       body: const Center(
-          child: Image(
-          image: NetworkImage(
-      'https://images.unsplash.com/photo-1535332371349-a5d229f49cb5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1vb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    ),
-  ),
-),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {}, // Fonctionnalité vide
-          backgroundColor: Colors.blueGrey, // Couleur du bouton
-          child: const Text('Click'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hot Reload Test'),
+        backgroundColor: Colors.green,
       ),
+       body: Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset('sunrise1.jpg', width: 500),
+        Image.asset('sunrise2.jpg', width: 500),
+      ],
+    ),
+    const SizedBox(height: 20),
+    Stack(
+      children: [
+        Image.asset('sunrise3.jpg', width: 500),
+        const Positioned(
+          top: 10,
+          left: 10,
+          child: Text(
+            'Stack Example',
+            style: TextStyle(color: Colors.white, backgroundColor: Colors.black),
+          ),
+        ),
+      ],
+    ),
+  ],
+),
+
     );
   }
 }
