69a3165821d4206aed98e0284011facb7e61fa62
feki_mohamed <medfeki33@gmail.com>
2024-12-15 18:22:52 +0100

    .

--- tp1/activite1/lib/main.dart
+++ tp1/activite1/lib/main.dart
@@ -1,36 +1,30 @@
 import 'package:flutter/material.dart';
 
-
-
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Exemple du Widget TextStyle'),
+          title: const Text('Exemple avec Polices Personnalisées'),
         ),
         body: const Center(
-          child: Text(
-            'Bonjour, Flutter !',
-            style: TextStyle(
-              color: Colors.green, // Couleur du texte
-              fontSize: 24.0, // Taille de la police
-              fontWeight: FontWeight.w900, // Très gras
-              fontStyle: FontStyle.italic, // Italique
-              letterSpacing: 2.0, // Espacement entre les lettres
-              wordSpacing: 5.0, // Espacement entre les mots
-              backgroundColor: Colors.yellow, // Couleur de fond du texte
-              decoration: TextDecoration.underline, // Décoration : souligné
-              decorationColor: Colors.red, // Couleur de la décoration
-              decorationStyle: TextDecorationStyle.dashed, // Style de la décoration
-              shadows: [
-                Shadow(
-                  color: Colors.grey, // Couleur de l'ombre
-                  offset: Offset(2, 2), // Décalage horizontal et vertical
-                  blurRadius: 3, // Rayon de flou
-                ),
-              ],
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Texte avec Roboto',
+                style: TextStyle(fontFamily: 'Roboto', fontSize: 24),
+              ),
+              SizedBox(height: 20), // Espacement entre les textes
+              Text(
+                'Texte avec Arial',
+                style: TextStyle(fontFamily: 'Arial', fontSize: 24),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Texte avec Courier',
+                style: TextStyle(fontFamily: 'Courier', fontSize: 24),
+              ),
+            ],
           ),
         ),
       ),
     ));
-
