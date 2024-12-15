addde7724af3965f4d9372d31713f7f87b203f7c
feki_mohamed <medfeki33@gmail.com>
2024-12-15 20:17:45 +0100

    *

--- tp1/activité2/activite2/lib/main.dart
+++ tp1/activité2/activite2/lib/main.dart
@@ -1,29 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Ma première Application'),
+          title: const Text('Ma première Application'),
           centerTitle: true,
           backgroundColor: Colors.blueGrey, // Couleur de l'AppBar
         ),
-        body: Center(
-          child: Text(
-            'Bonjour troisième année',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              color: Colors.grey[500],
-              fontFamily: 'Indie',
-            ),
-          ),
-        ),
+       body: const Center(
+          child: Image(
+          image: NetworkImage(
+      'https://images.unsplash.com/photo-1535332371349-a5d229f49cb5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG1vb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    ),
+  ),
+),
         floatingActionButton: FloatingActionButton(
           onPressed: () {}, // Fonctionnalité vide
           backgroundColor: Colors.blueGrey, // Couleur du bouton
