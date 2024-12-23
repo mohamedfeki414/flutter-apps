025d2bbbcaaa33dcc573dc6b73f410cd84b140e5
feki_mohamed <medfeki33@gmail.com>
2024-12-23 19:31:45 +0100

    .

--- tp6/act6/lib/main.dart
+++ tp6/act6/lib/main.dart
@@ -1,41 +1,49 @@
-import 'package:firebase_core/firebase_core.dart'; 
-import 'package:flutter/material.dart'; 
-import 'package:cloud_firestore/cloud_firestore.dart'; 
- 
-void main() async { 
-  WidgetsFlutterBinding.ensureInitialized(); 
-  await Firebase.initializeApp(); 
-  runApp(MyApp()); 
-}
-  @override 
-  Widget build(BuildContext context) { 
-    return MaterialApp( 
-      home: Scaffold( 
-        appBar: AppBar( 
-          title: Text('Firebase Firestore Example'), 
-        ), 
-        body: Center( 
-          child: ElevatedButton( 
-            onPressed: addTeacher, 
-            child: Text('Ajouter Enseignant'), 
-          ), 
-        ), 
-      ), 
-    ); 
-  } 
-} 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
- 
-void addTeacher() { 
-  FirebaseFirestore.instance.collection('enseignant').add({ 
-    'Id_enseignant': '12345', 
-    'nom': 'Ali', 
-    'prenom': 'Ben Salah', 
-    'grade': 'Professeur', 
-    'specialite': 'Informatique', 
-  }).then((value) { 
-    print("Enseignant ajouté avec succès !"); 
-  }).catchError((error) { 
-    print("Erreur lors de l'ajout de l'enseignant : $error"); 
-  }); 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Firebase Firestore Example'),
+        ),
+        body: const Center(
+          child: ElevatedButton(
+            onPressed: addTeacher,
+            child: Text('Ajouter Enseignant'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void addTeacher() {
+  FirebaseFirestore.instance.collection('enseignant').add({
+    'Id_enseignant': '12345',
+    'nom': 'Ali',
+    'prenom': 'Ben Salah',
+    'grade': 'Professeur',
+    'specialite': 'Informatique',
+  }).then((value) {
+    if (kDebugMode) {
+      print("Enseignant ajouté avec succès !");
+    }
+  }).catchError((error) {
+    if (kDebugMode) {
+      print("Erreur lors de l'ajout de l'enseignant : $error");
+    }
+  });
 }
